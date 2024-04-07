25e6c6e82249403b4a494133cc279f51604f5db7
Kima018 <marijan.dev@gmail.com>
2024-04-07 16:12:27 +0200

    working on layout.blade.php to show content inside

---
+++ resources/views/footer.blade.php
@@ -0,0 +1,5 @@
+<footer class="bg-indigo-100 py-5">
+    <div class="container mx-auto flex justify-center">
+        <h3 class="font-light underline underline-offset-4 hover:text-purple-700 hover:font-serif cursor-pointer transition-all ">Stranicu radi Maki@flow</h3>
+    </div>
+</footer>
---
+++ resources/views/navigation.blade.php
@@ -0,0 +1,27 @@
+<nav class="bg-gray-800 border-gray-200 px-2 sm:px-4 py-2.5">
+    <div class="container flex justify-between items-center mx-auto">
+        <a href="/">
+            <span class="self-center text-lg font-semibold whitespace-nowrap dark:text-white">MakiFlow</span>
+        </a>
+        <div class="flex">
+            <ul class="flex  mt-4 md:flex-row md:space-x-8 md:mt-0 md:text-sm md:font-medium">
+                <li>
+                    <a href="/"
+                       class="block py-2 pr-4 pl-3 text-white bg-blue-700 rounded md:bg-transparent md:text-blue-700 md:p-0 dark:text-white">Home</a>
+                </li>
+                <li>
+                    <a href="/about"
+                       class="block py-2 pr-4 pl-3 text-white bg-blue-700 rounded md:bg-transparent md:text-blue-700 md:p-0 dark:text-white">About</a>
+                </li>
+                <li>
+                    <a href="/shop"
+                       class="block py-2 pr-4 pl-3 text-white bg-blue-700 rounded md:bg-transparent md:text-blue-700 md:p-0 dark:text-white">Shop</a>
+                </li>
+                <li>
+                    <a href="/contact"
+                       class="block py-2 pr-4 pl-3 text-white bg-blue-700 rounded md:bg-transparent md:text-blue-700 md:p-0 dark:text-white">Contact</a>
+                </li>
+            </ul>
+        </div>
+    </div>
+</nav>
