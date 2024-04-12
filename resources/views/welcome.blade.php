1282672527ee79cd08e4f32d4713a02e60f7fdfc
Kima018 <marijan.dev@gmail.com>
2024-04-12 16:57:23 +0200

    predavanje 10, auth dodata, auth middleware dodat za admin rute

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -7,7 +7,7 @@
             <div class="mx-auto max-w-2xl py-32 sm:py-48 lg:py-56">
 
                 <div class="text-center">
-                    <h1 class="text-4xl font-bold tracking-tight text-gray-900 sm:text-6xl mb-5">Home blade</h1>
+                    <h1 class="text-4xl font-bold tracking-tight text-gray-900 sm:text-6xl mb-5">Dashboard blade</h1>
                     <h2 class="text-2xl font-bold tracking-tight text-gray-900">{{$currentTime}}</h2>
                     <h2 class="text-2xl font-bold tracking-tight text-gray-900">trenutno je {{$hours}} sati</h2>
                     <p class="mt-6 text-lg leading-8 text-gray-600">Anim aute id magna aliqua ad ad non deserunt sunt.
--- routes/web.php
+++ routes/web.php
@@ -3,6 +3,7 @@
 use App\Http\Controllers\ContactController;
 use App\Http\Controllers\HomepageController;
 use App\Http\Controllers\OceneController;
+use App\Http\Controllers\ProfileController;
 use App\Http\Controllers\ShopController;
 use Illuminate\Support\Facades\Route;
 
@@ -14,26 +15,39 @@ Route::get("/shop", [ShopController::class, 'index']);
 // predavanje 6
 Route::get("/ocene", [OceneController::class, 'index']);
 Route::get("/dodaj-ocenu", [OceneController::class, 'dodajOcenu']);
-Route::post("/nova-ocena",[OceneController::class,"novaOcena"]);
+Route::post("/nova-ocena", [OceneController::class, "novaOcena"]);
 
 //
-
-Route::get("/admin/all-contacts", [ContactController::class, 'getAllContacts'])->name('adminAllContacts');
+Route::middleware('auth')->group(function () {
+    Route::get("/admin/all-contacts", [ContactController::class, 'getAllContacts'])->name('adminAllContacts');
 //Route::get("/admin/products", [ShopController::class, 'getAllProducts']);
-Route::get("/admin/add-product", [ShopController::class, "addProduct"]);
-Route::get("/admin/all-products",[ShopController::class,'adminAllProducts'])->name('adminAllProducts');
-Route::get("/admin/delete-product/{product}",[ShopController::class,"delete"])->name('deleteProduct');
-Route::get("/admin/delete-contact/{contact}",[ContactController::class,'delete'])->name('deleteContact');
+    Route::get("/admin/add-product", [ShopController::class, "addProduct"]);
+    Route::get("/admin/all-products", [ShopController::class, 'adminAllProducts'])->middleware('auth')->name('adminAllProducts');
+    Route::get("/admin/delete-product/{product}", [ShopController::class, "delete"])->name('deleteProduct');
+    Route::get("/admin/delete-contact/{contact}", [ContactController::class, 'delete'])->name('deleteContact');
 
 
 //domaci 8
-Route::get("/admin/product/{product}",[ShopController::class,'singleProduct'])->name('product.single');
-Route::post("/admin/update-product",[ShopController::class,'update'])->name('updateProduct');
-Route::get("/admin/contact/{contact}",[ContactController::class,'singleContact'])->name('contact.single');
-Route::post("/admin/update-contact",[ContactController::class,'update'])->name('updateContact');
+    Route::get("/admin/product/{product}", [ShopController::class, 'singleProduct'])->name('product.single');
+    Route::post("/admin/update-product", [ShopController::class, 'update'])->name('updateProduct');
+    Route::get("/admin/contact/{contact}", [ContactController::class, 'edit'])->name('contact.single');
+    Route::post("/admin/update-contact", [ContactController::class, 'update'])->name('updateContact');
 
 //
+    Route::post("/admin/send-contact", [ContactController::class, "sendContact"]);
+    Route::post("/admin/send-product", [ShopController::class, "sendProduct"]);
 
-Route::post("/admin/send-contact", [ContactController::class, "sendContact"]);
-Route::post("/admin/send-product", [ShopController::class, "sendProduct"]);
+});
 
+
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
+
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+});
+
+require __DIR__ . '/auth.php';
